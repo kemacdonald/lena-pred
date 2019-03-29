35c676814ecebebdc47a9a467732d87ba9ba6613
Kyle MacDonald <kylem412@gmail.com>
2019-03-29 16:44:36 -0700

    added code to reanalyze rasanen .mat files

---
+++ code/03_analysis/analyze_rasanen_coefs.R
@@ -0,0 +1,20 @@
+library(R.matlab)
+library(tidyverse)
+
+d <- readMat("~/Downloads/results_19-Oct-2017 21_07_23_ManyBabies_usesyllables0_framesize_100.mat")
+
+# check okko's coefs
+list_to_df <- function(list_item) {
+  list_item[[1]] %>% as_tibble(.name_repair = NULL)
+}
+
+d_coefs <- d$F0PARAMS %>% map_df(list_to_df)
+
+# plot the coefs and cluster assignments 
+clusters <- kmeans(as.matrix(d_coefs), centers = 8 )
+d_coefs %>% mutate(cluster = clusters$cluster) -> d_coefs
+ggplot(d_coefs, aes(V1, V2, color = cluster %>% as_factor())) + 
+  geom_point()
+
+# plot the distribution of clusters
+d_coefs %>% count(cluster) %>% ggplot(aes(cluster, n)) + geom_bar(stat = 'identity')
